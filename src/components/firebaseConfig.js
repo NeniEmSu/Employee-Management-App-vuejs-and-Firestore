9e58e70c2995b702298aed4561dcd86798589e78
Neni EmSu <35854224+cryptodoct0r@users.noreply.github.com>
2019-04-24 10:37:23 +0300

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyClQPCWgrYeUN7bFI48UksQY0LuwEoWkj8",
+    authDomain: "vuefs-prod-c5042.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-c5042.firebaseio.com",
+    projectId: "vuefs-prod-c5042",
+    storageBucket: "vuefs-prod-c5042.appspot.com",
+    messagingSenderId: "994297092886"
+}
