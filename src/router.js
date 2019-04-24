7d8402cfd582a210d14dbef6593198be8bf4e912
Neni EmSu <35854224+cryptodoct0r@users.noreply.github.com>
2019-04-24 12:01:55 +0300

    added and imported material design

--- src/router.js
+++ src/router.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Material from 'materialize-css'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
 import Dashboard from '@/components/Dashboard'
 import NewEmployee from '@/components/NewEmployee'
 import ViewEmployee from '@/components/ViewEmployee'
 import EditEmployee from '@/components/EditEmployee'
 import Navbar from '@/components/Navbar'
 
+
 Vue.use(Router)
+Vue.use(Material)
 
 export default new Router({
   routes: [{
