ed1aa0670b66f65c50c0e1179b7289ec228972e3
Neni EmSu <35854224+cryptodoct0r@users.noreply.github.com>
2019-04-24 14:06:02 +0300

    fetched and putput data from database

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="dashboard">
-    <h3>Dashboard</h3>
+    <ul class="collection with-header">
+      <li class="collection-header"><h4>Employees</h4></li>
+      <li v-for="employee in employees" v-bind::key="employee.id" class="collection-item">
+        <div class="chip">{{employee.dept}}</div>
+        {{employee.employee_id}}:{{employee.name}}
+
+        <router-link class="secondary-content" :to="{name: 'view-employee', params: {employee_id: employee.employee_id}}"><i class="fa fa-eye"></i></router-link>
+      </li>
+    </ul>
     <div class="fixed-action-btn">
       <router-link to="/new" class="btn-floating btn-large red">
         <i class="fa fa-plus"></i>
@@ -10,10 +18,29 @@
 </template>
 
 <script>
+import db from "./firebaseInit";
 export default {
   name: "dashboard",
   data() {
-    return {};
+    return {
+      employees: []
+    };
+  },
+  created() {
+    db.collection("employees")
+      .orderBy('dept').get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          const data = {
+            id: doc.id,
+            employee_id: doc.data().employee_id,
+            name: doc.data().name,
+            dept: doc.data().dept,
+            position: doc.data().position
+          };
+          this.employees.push(data);
+        });
+      });
   }
 };
 </script>
